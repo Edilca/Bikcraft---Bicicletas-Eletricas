204288d0d2c8c10b2f00354447eee931b1a41376
Edilca <edilca@gmail.com>
2022-04-21 10:13:37 -0300

    pequenas alteracoes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-const links = document.querySelectorAll('.header-menu a');
+// Ativar Links do Menu
+const links = document.querySelectorAll(".header-menu a");
 
-function ativarLink(links) {
+function ativarLink(link) {
   const url = location.href;
-  const href = links.href;
-
+  const href = link.href;
   if (url.includes(href)) {
-    links.classList.add('ativo');
+    link.classList.add("ativo");
   }
 }
+
 links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
